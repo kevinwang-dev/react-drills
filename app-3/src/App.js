76d4a39ac6ae388f7ad62d5b70e12dfab306066f
kevin <kevin.wang8687@gmail.com>
2020-12-05 14:38:15 -0800

    completed app 3

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      filterString: "",
+      words: ["cake", "candle", "juice", "water", "pasta", "butter", "beans"],
+    };
+  }
+
+  handleChange(value) {
+    this.setState({ filterString: value });
+  }
+
+  render() {
+    let wordsToShow = this.state.words
+      .filter((e, i) => {
+        return e.includes(this.state.filterString);
+      })
+      .map((e, i) => {
+        return <h2>{e}</h2>;
+      });
+
+    return (
+      <div className="App">
+        <input
+          type="text"
+          onChange={(e) => {
+            this.handleChange(e.target.value);
+          }}
+        />
+        {wordsToShow}
+      </div>
+    );
+  }
 }
 
 export default App;
